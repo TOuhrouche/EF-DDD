b09f5115a782c31b846103c9f7c8f8d07d8952aa
Tahar <tahar@stockandbuy.com>
2022-08-20 19:51:43 +0200

    Cleanup

--- ConsoleApp1/Partnership/PartnerEmployeeConfiguration.cs
+++ ConsoleApp1/Partnership/PartnerEmployeeConfiguration.cs
@@ -10,9 +10,8 @@ namespace EF_DDD.Partnership
             builder.ToTable("Person");
             builder.HasKey(c => c.Id);
             builder.Property(c => c.Id).HasColumnName("Id");
-            //builder.Property(c => c.ContractorId).HasColumnName("ContractorId");
+            builder.Property(c => c.ContractorId).HasColumnName("ContractorId");
             builder.Property<int?>("PartnerId").HasColumnName("CompanyId");
-            builder.Property<int?>("Contractor_CompanyId").HasColumnName("CompanyId");
             builder.HasOne<Contractor>().WithOne().HasForeignKey<PartnerEmployee>(c => c.Id);
         }
     }
